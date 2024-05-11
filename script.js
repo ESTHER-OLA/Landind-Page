6cee369364fab0cddecef5ff37a1f5011ee2204d
techeo-p <estherola2018@gmail.com>
2024-05-11 16:49:01 +0100

    updated code

--- script.js
+++ script.js
@@ -1,6 +1,38 @@
-const navToggle = document.querySelector(".nav-toggle");
-const navMenu = document.querySelector(".container");
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
 
-navToggle.addEventListener("click", () => {
-  navMenu.classList.toggle("active");
+    const targetId = this.getAttribute("href");
+    const targetElement = document.querySelector(targetId);
+    const offset = 100; // the offset
+
+    if (targetElement) {
+      const offsetPosition = targetElement.offsetTop - offset;
+
+      window.scrollTo({
+        top: offsetPosition,
+        behavior: "smooth",
+      });
+    }
+  });
 });
+
+function showSidebar() {
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.style.display = "flex";
+}
+
+function hideSidebar() {
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.style.display = "none";
+
+  const sidebarLinks = document.querySelectorAll(".sidebar a");
+  sidebarLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+      sidebar.style.display = "none"; // Close the sidebar
+    });
+  });
+}
+
+// Call the hideSidebar function after defining it to apply the changes when the page loads
+hideSidebar();
